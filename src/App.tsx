428db975ed3c0349927f92ec5c31cdb9722c3ffb
Alex Santos <devalexsantos@gmail.com>
2024-02-21 10:13:15 -0300

    first commit: setup project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button"
+
+function App() {
+  return (
+    <div className="bg-red-500">
+      Hello Water
+      <Button>Button</Button>
+    </div>
+  )
+}
+
+export default App
