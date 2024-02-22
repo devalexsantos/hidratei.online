a383ee6f0c3c7c52a73f863197bdd400d7a85679
Alex Santos <devalexsantos@gmail.com>
2024-02-22 14:08:08 -0300

    chore: add vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './globals.css'
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from './components/theme-provider.tsx'
 import { WaterConsumeContextProvider } from './contexts/WaterConsume.tsx'
 
@@ -10,6 +11,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <WaterConsumeContextProvider>
         <App />
+        <Analytics />
       </WaterConsumeContextProvider>
     </ThemeProvider>
   </React.StrictMode>,
